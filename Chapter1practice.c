34054fc43f13741de2a5c6c6d4ba46256fcf1d6b
Pragya <prachauhan12@gmail.com>
2026-04-10 18:11:44 +0530

    Chapter 1 Practice set , tried few questions on the topics covered in chapter 1

---
+++ Chapter1practice.c
@@ -0,0 +1,32 @@
+//problem 1 
+// calculate area of rectangle using hard coded values
+// #include<stdio.h>
+// int main() {
+//     int a, b;
+//     // a =3;
+//     // b =4;
+//     scanf("Enter length and breadth of rectangle: %d %d", &a, &b);
+//     printf("Area of rectangle: %d", a*b);
+//     return 0;
+// }
+
+//problem 2
+// calculate paramter of circle using hard coded values
+// #include<stdio.h>
+// int main(){
+//     int r=6;
+//     printf("para of circle is : %d is %f", r, 2*3.14*r);
+//     return 0;
+// }
+
+
+//problem 3
+//calculate area of cylinder using hard coded values with area of circle 
+// #include<stdio.h> 
+// int main() {
+//     int r=3, h=7;
+//     printf("area of circle is: %d is %f\n" , r, 3.14*r*r);
+//     printf("Area of cylinder %d is %f", r, h, 3.14*r*r*h);
+//     return 0;
+
+// }
