2f753bbe9591640a8c1bb8d234286eb902cb1b0d
Pragya <prachauhan12@gmail.com>
2026-04-10 18:10:30 +0530

    Completed chpater 1 - VAriables, Constant, Datatypes, keywords, input, Escape Sequence

---
+++ Chapter1.c
@@ -0,0 +1,57 @@
+//Variables
+#include<stdio.h>
+// int main() {
+//     int a, b; //declare
+//     a =6; //initialize
+//     printf("Hello, World! %d ", a); //%d used to print integer values
+//     return 0;
+// }
+
+
+// ---------------------------
+//datatypes
+// int main() {
+//     int a =4; //memomry it takes -- 4 bytes 32 bits
+//     float b =  6.2; // memory it takes -- 4 bytes 32 bits
+//     char c = 'A'; // memory it takes -- 1 byte 8 bits
+//     printf("Hello, World! %d %f %c", a, b, c);
+//     return 0;
+// }
+
+
+//---------------------------
+// we can also check size of data types using sizeof operator
+
+// int main() {
+//     printf("Size of int: %d bytes\n", sizeof(int));
+//     printf("Size of float: %d bytes\n", sizeof(float));
+//     printf("Size of char: %d bytes\n", sizeof(char));
+//     return 0;
+// }
+
+//---------------------------
+//Input
+
+// int main() {
+//     int a;
+//     scanf("%d", &a); //& is used to get the address of variable a
+//     printf("You entered: %d", a);
+
+//     return 0;
+// }
+
+//---------------------------
+/* Escape sequences
+-> newline character,
+-> \t is used for tab space
+-> \" is used to print double quote
+-> \\ is used to print backslash
+
+*/
+// int main() {
+//     char ch ='\n'; // newline character
+
+//     printf("Enter a character: \" new line ");
+
+// return 0;
+// }
