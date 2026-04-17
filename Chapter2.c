a814d4568ed710838938cfbe1e181858731808d7
Pragya <prachauhan12@gmail.com>
2026-04-17 22:25:45 +0530

    Updated doc

--- Chapter2.c
+++ Chapter2.c
@@ -40,18 +40,31 @@
 // }
 
 //Associativity in C
-#include<stdio.h>
-int main() {
-    int a =3, b=6, c=9;
-    printf("the value is %d\n", a*b/c + 7);
-    printf(" the value is %d", 3*b/2*c + 7*a);
-    //3*b/2*c + 7*a -- firstly staars will evaluated.  --- (3*b)/2*c + 7*a  
-    // -> 3*6/2*9 + 7*3 
-    // -> 18/2*9 + 21
-    // -> 9*9 + 21
-    // -> 81 + 21
-    // -> 102
+// #include<stdio.h>
+// int main() {
+//     int a =3, b=6, c=9;
+//     printf("the value is %d\n", a*b/c + 7);
+//     printf(" the value is %d", 3*b/2*c + 7*a);
+//     //3*b/2*c + 7*a -- firstly staars will evaluated.  --- (3*b)/2*c + 7*a  
+//     // -> 3*6/2*9 + 7*3 
+//     // -> 18/2*9 + 21
+//     // -> 9*9 + 21
+//     // -> 81 + 21
+//     // -> 102
     
-    return 0;
+//     return 0;
 
-}
+// }
+
+// to change datatype
+// #include<stdio.h>
+// int main() {
+//     int n = 45;
+//     float m = 45.33;
+
+//     n = (int)m; // this will convert the float value of m to an integer and assign it to n.
+//     // The decimal part will be truncated, so n will become 45.
+//     printf("the value of n is %d\n", n); // output will be 45.
+//     return 0;
+// }
+
